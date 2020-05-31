956e76ea730b7e53cd295f8ae03f3c4b299d0a90
vanmrqs <vanessa2011164@hotmail.com>
2020-05-31 16:56:20 -0300

    Adicionados retornos em CandidatoController

--- app/Http/Controllers/CandidatoController.php
+++ app/Http/Controllers/CandidatoController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 
 use App\Candidato;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 
@@ -35,15 +36,19 @@ class CandidatoController extends Controller {
      * Cadastra um novo candidato
      *
      * @param Request $request
+     * @return JsonResponse
      * @throws ValidationException
      */
     public function store(Request $request){
         $candidato = $this->validate($request, Candidato::$rules);
 
-        // TODO: Verificar se CPF é valido
         $candidato['cpfCandidato'] = $this->removeNaoNumericos($candidato['cpfCandidato']);
+        if ( ! $this->validaCPF($candidato['cpfCandidato']) ) {
+            return response()->json(['message' => 'CPF inválido'], 400);
+        }
 
         Candidato::create($candidato);
+        return response()->json(['message' => 'success'], 200);
     }
 
     /**
@@ -51,28 +56,32 @@ class CandidatoController extends Controller {
      *
      * @param Request $request
      * @param $codCandidato
+     * @return JsonResponse
      * @throws ValidationException
      */
     public function update(Request $request, int $codCandidato){
         $this->validate($request, Candidato::$rules);
 
-        //TODO: Verificar se cpf é válido
+        $request->cpfCandidato = $this->removeNaoNumericos($request->cpfCandidato);
+        if ( ! $this->validaCPF($request->cpfCandidato) ) {
+            return response()->json(['message' => 'CPF inválido'], 400);
+        }
+
         $candidato = Candidato::findOrFail($codCandidato);
-        $candidato['nomeCandidato']           = $request->nomeCandidato;
-        $candidato['cpfCandidato']            = $this->removeNaoNumericos($request->cpfCandidato);
-        $candidato['rgCandidato']             = $request->rgCandidato;
-        $candidato['dataNascimentoCandidato'] = $request->dataNascimentoCandidato;
-        $candidato->save();
+        $candidato->update($request->all());
+        return response()->json(['message' => 'success'], 200);
     }
 
     /**
      * Exclui um candidato
      *
      * @param int $codCandidato
+     * @return JsonResponse
      */
     public function destroy(int $codCandidato){
         // TODO: Excluir usuário
         Candidato::destroy($codCandidato);
+        return response()->json(['message' => 'success'], 200);
     }
 
     /**
@@ -96,5 +105,33 @@ class CandidatoController extends Controller {
      */
     private function removeNaoNumericos(string $texto){
         return preg_replace('/[^0-9]/', '', $texto);
+    }
+
+    /**
+     * Retorna se um cpf é válido
+     *
+     * @param string $cpf
+     * @return bool
+     */
+    private function validaCPF(string $cpf) {
+        if ( strlen($cpf) != 11 ) {
+            return false;
+        }
+
+        if ( preg_match('/(\d)\1{10}/', $cpf) ) {
+            return false;
+        }
+
+        for ($t = 9; $t < 11; $t++) {
+            for ($d = 0, $c = 0; $c < $t; $c++) {
+                $d += $cpf{$c} * (($t + 1) - $c);
+            }
+            $d = ((10 * $d) % 11) % 10;
+            if ($cpf{$c} != $d) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
